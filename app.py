b42fa142cd4081674541e7da6c09e51383e7f57c
J. Nathan Scott <scottjn@gmail.com>
2019-03-28 21:35:09 -0600

    trying to get input from the HTML form

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template, request, redirect
 
+# Qu xcpuajeyffeoNLoSEozx
+# 
+# 
+# GET https://www.quandl.com/api/v3/datasets/{database_code}/{dataset_code}/data.{return_format}
+# curl "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=xcpuajeyffeoNLoSEozx"
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+@app.route('/proc_symbol', methods=['POST'])
+def hello():
+    user_symbol = request.form['user_symbol']
+    return user_symbol
 
 @app.route('/about')
 def about():
@@ -12,3 +24,5 @@ def about():
 
 if __name__ == '__main__':
   app.run(port=33507)
+
+
